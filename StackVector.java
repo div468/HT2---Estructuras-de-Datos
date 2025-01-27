78e5867257b29ceeb7bff2c56e072384af507f27
div468 <divasjulian2019ig@gmail.com>
2025-01-27 17:56:11 -0600

    Creación del Stack usando Vector

---
+++ StackVector.java
@@ -0,0 +1,37 @@
+import java.util.Vector;
+
+public class StackVector<E> implements Stack<E> {
+    protected Vector <E> datos;
+
+    public StackVector(){
+        datos = new Vector<E>();
+    }
+    public void push (E item){
+        // pre: 
+        // post: item is added to stack
+        // will be popped next if no intervening push
+        datos.add(item);
+    }
+
+    public E pop() {
+        // pre: stack is not empty
+        // post: most recently pushed item is removed and returned
+        return datos.remove(size()-1);
+    }
+
+    public E peek() {
+        // pre: stack is not empty
+        // post: top value (next to be popped) is returned
+        return datos.get(size()-1);
+    }
+
+    public int size(){
+        // post: returns true if and only if the stack is empty
+        return datos.size();
+    }
+
+    public boolean empty(){
+        // post: returns the number of elements in the stack
+        return datos.size() == 0;
+    }
+}
